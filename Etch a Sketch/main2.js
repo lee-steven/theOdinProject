7cccc85d812ecd3304c97c8574a8d11e56bd8eed
Steven Lee <lee.steven@wustl.edu>
2019-07-27 15:06:20 -0400

    Done w/ redo practice

--- Etch a Sketch/main2.js
+++ Etch a Sketch/main2.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 
+//Setting Up Default Grid
 createDiv(16);
+addMouseHover(); 
 
 function createDiv(number){
     for(let i = 0; i < Math.pow(number,2); i++){
@@ -10,7 +12,19 @@ function createDiv(number){
     }
 }
 
-function mouseListen(){
+//Useful Answer from Stack Overflow:
+// https://stackoverflow.com/questions/52563263/using-the-css-grid-repeat-declaration-in-javascript
+function setGrid(number){
+    // container.setAttribute('style', 'grid-template-columns: repeat(' + number + ', 1fr)');
+    // container.setAttribute('style', 'grid-template-rows: repeat(' + number + ', 1fr)');
+    container.style.setProperty('--gridSize', number);
+}
+
+// Notes for Myself: 
+// Tried to use getElementByClassName but didn't work got an error message, had to use querySelectorAll instead
+// According to https://www.reddit.com/r/learnjavascript/comments/884hc1/getelementsbyclassname_vs_queryselectorall/ 
+// "document.GetElementsByClassName returns a HTMLCollection object which does not have the forEach method" which is why it wasn't working for me
+function addMouseHover(){
     var gridBoxes = document.querySelectorAll('.box');
     gridBoxes.forEach((gridBox) => {
         gridBox.addEventListener("mouseenter", (e) => {
@@ -38,6 +52,7 @@ newGridBtn.addEventListener("click", ()=> {
     var newGridSize = prompt("Enter a new grid size!");
     deleteChild();
     createDiv(newGridSize);
-    mouseListen();
+    addMouseHover();
+    setGrid(newGridSize);
 });
 
